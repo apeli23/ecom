93f4c982c57bd1d16153fc09462926a640196540
Brian Samwel Apeli <brianapeli@yahoo.com>
2018-11-12 08:52:15 +0300

    Third commit-with repaired admin.py forms for the models. can be viewed through admin page

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,39 +5,51 @@ from .models import Product,category
 from .forms import productAdminForm
 
 class productAdmin(admin.ModelAdmin):
-	form = productAdminForm
-
-	# sets values for how the admin site lists your products
-	fields = ('name','price','slug','old_price','created_at','updated_at',)
-	fields_links = ('name',)
-	list_per_page = 50
-	ordering = ['-created_at']
-	search_fields = ['name', 'description', 'meta_keywords', 'meta_description']
-	exclude = ('created_at', 'updated_at',)
-	
-	# sets up slug to be generated from product name
-	prepopulated_fields = {'slug' : ('name',)}
-
-
 	class Meta:
 		model = Product
-		verbose_name_plural = 'Products'
 
 admin.site.register(Product, productAdmin)
 
-class categoryAdmin(admin.ModelAdmin):
-		
-	#sets up values for how admin site lists categories
-	list_display = ('name', 'created_at', 'updated_at',)
-	list_display_links = ('name',)
-	list_per_page = 20
-	ordering = ['name']
-	search_fields = ['name', 'description', 'meta_keywords', 'meta_description']
-	exclude = ('created_at', 'updated_at',)
 
-	# sets up slug to be generated from category name
-	prepopulated_fields = {'slug' : ('name',)}
+class categoryAdmin(admin.ModelAdmin):
 	class Meta:
 		model = category
 
 admin.site.register(category, productAdmin)
+# class productAdmin(admin.ModelAdmin):
+# 	form = productAdminForm
+
+# 	# sets values for how the admin site lists your products
+# 	fields = ('name','price','old_price','created_at','updated_at',)
+# 	fields_links = ('name',)
+# 	list_per_page = 50
+# 	ordering = ['-created_at']
+# 	search_fields = ['name', 'description', 'meta_keywords', 'meta_description']
+# 	exclude = ('created_at', 'updated_at',)
+	
+# 	# sets up slug to be generated from product name
+# 	prepopulated_fields = {'slug' : ('name',)}
+
+
+# 	class Meta:
+# 		model = Product
+# 		verbose_name_plural = 'Products'
+
+# admin.site.register(Product, productAdmin)
+
+# class categoryAdmin(admin.ModelAdmin):
+		
+# 	#sets up values for how admin site lists categories
+# 	list_display = ('name', 'created_at', 'updated_at',)
+# 	list_display_links = ('name',)
+# 	list_per_page = 20
+# 	ordering = ['name']
+# 	search_fields = ['name', 'description', 'meta_keywords', 'meta_description']
+# 	exclude = ('created_at', 'updated_at',)
+
+# 	# sets up slug to be generated from category name
+# 	prepopulated_fields = {'slug' : ('name',)}
+# 	class Meta:
+# 		model = category
+
+# admin.site.register(category, productAdmin)
